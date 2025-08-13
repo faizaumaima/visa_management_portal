2d8f292138823c6b5ffe56c9d0246ecff83f0083
hridi61 <hridi042@gmail.com>
2025-08-14 00:49:11 +0600

    initial commit

--- src/main/java/com/example/ms3_group47_evisa/CustomerSupportAgency/RespondToTicketController.java
+++ src/main/java/com/example/ms3_group47_evisa/CustomerSupportAgency/RespondToTicketController.java
@@ -1,5 +1,13 @@
 package com.example.ms3_group47_evisa.CustomerSupportAgency;
 
+import com.example.ms3_group47_evisa.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class RespondToTicketController
 {
     @javafx.fxml.FXML
@@ -21,5 +29,15 @@ public class RespondToTicketController
 
     @javafx.fxml.FXML
     public void handleSendResponseButtonOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CustomerSupportAgency/RespondToTicket.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            ///
+        }
     }
 }
