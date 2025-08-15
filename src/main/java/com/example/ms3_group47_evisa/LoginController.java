7968175faa5b7edc38fd2bf5e02689c74e4f7a8c
hridi61 <hridi042@gmail.com>
2025-08-15 15:55:15 +0600

    initial commit

--- src/main/java/com/example/ms3_group47_evisa/LoginController.java
+++ src/main/java/com/example/ms3_group47_evisa/LoginController.java
@@ -1,37 +1,17 @@
 package com.example.ms3_group47_evisa;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class LoginController {
-    @javafx.fxml.FXML
-    private TextField userIdTextField;
+public class LoginController
+{
     @javafx.fxml.FXML
     private TextField passwordTextfield;
+    @javafx.fxml.FXML
+    private TextField userIdTextField;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }
-
-    @Deprecated
-    public void loginOnaction(ActionEvent actionEvent) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ApplicationIntakeOfficer/ApplicationIntakeOfficerDashboard.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            Stage stage = new Stage();
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            ///
-        }
     }
 
     @javafx.fxml.FXML
@@ -39,15 +19,6 @@ public class LoginController {
     }
 
     @javafx.fxml.FXML
-    public void handleCreateAccountOnAction(ActionEvent actionEvent) throws IOException {
-        Parent root = null ;
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CreateAccountPage.fxml"));
-        root = fxmlLoader.load();
-
-        Scene scene = new Scene(root) ;
-        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Create Account");
-        stage.show();
+    public void handleCreateAccountOnAction(ActionEvent actionEvent) {
     }
 }
