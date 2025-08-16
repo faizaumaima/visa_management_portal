6f7b675a235c37e77aeb413eeacb811911abb053
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-08-16 12:41:38 +0600

    initial commit

--- src/main/java/com/example/ms3_group47_evisa/FinancialOfficer/PaymentVerificationClass.java
+++ src/main/java/com/example/ms3_group47_evisa/FinancialOfficer/PaymentVerificationClass.java
@@ -1,4 +1,48 @@
 package com.example.ms3_group47_evisa.FinancialOfficer;
 
 public class PaymentVerificationClass {
+        private int id;
+        private double amount;
+        private String status;
+
+        public PaymentVerificationClass(int id, double amount, String status) {
+            this.id = id;
+            this.amount = amount;
+            this.status = status;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(double amount) {
+            this.amount = amount;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+
+        @Override
+        public String toString() {
+            return "Payment{" +
+                    "id=" + id +
+                    ", amount=" + amount +
+                    ", status='" + status + '\'' +
+                    '}';
+        }
+    }
 }
--- src/main/java/com/example/ms3_group47_evisa/FinancialOfficer/PendingRefundRequestsClass.java
+++ src/main/java/com/example/ms3_group47_evisa/FinancialOfficer/PendingRefundRequestsClass.java
@@ -1,4 +1,71 @@
 package com.example.ms3_group47_evisa.FinancialOfficer;
 
 public class PendingRefundRequestsClass {
+        private int requestId;
+        private String customerName;
+        private double refundAmount;
+        private String reason;
+        private String status;
+
+
+        public void PendingRefundRequest(int requestId, String customerName, double refundAmount, String reason, String status) {
+            this.requestId = requestId;
+            this.customerName = customerName;
+            this.refundAmount = refundAmount;
+            this.reason = reason;
+            this.status = status;
+        }
+
+
+        public int getRequestId() {
+            return requestId;
+        }
+
+        public void setRequestId(int requestId) {
+            this.requestId = requestId;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public double getRefundAmount() {
+            return refundAmount;
+        }
+
+        public void setRefundAmount(double refundAmount) {
+            this.refundAmount = refundAmount;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        public void setReason(String reason) {
+            this.reason = reason;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+        @Override
+        public String toString() {
+            return "PendingRefundRequest{" +
+                    "requestId=" + requestId +
+                    ", customerName='" + customerName + '\'' +
+                    ", refundAmount=" + refundAmount +
+                    ", reason='" + reason + '\'' +
+                    ", status='" + status + '\'' +
+                    '}';
+        }
+    }
+    
 }
